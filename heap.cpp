d194804346f9522e4cb9b867fc2841db2fa314df
JuiMin Hsu <alex.hsu1070344@gmail.com>
2024-11-29 13:18:47 +0800

    hi

--- heap.cpp
+++ heap.cpp
@@ -2,39 +2,32 @@
 using namespace std;
 
 void heapify(int arr[], int n, int i) {
-    int largest = i; // Initialize largest as root
+    int largest = i; // 設根為最大值
     int left = 2 * i + 1; // left = 2*i + 1
     int right = 2 * i + 2; // right = 2*i + 2
 
-    // If left child is larger than root
+    //比較根與左右子樹的值，找出最大值
     if (left < n && arr[left] > arr[largest])
         largest = left;
 
-    // If right child is larger than largest so far
     if (right < n && arr[right] > arr[largest])
         largest = right;
 
-    // If largest is not root
+    //如果最大值不是根，交換最大值與根
     if (largest != i) {
         swap(arr[i], arr[largest]);
-
-        // Recursively heapify the affected sub-tree
+        // 遞迴，重新檢查受影響的子樹
         heapify(arr, n, largest);
     }
 }
 
 void heapSort(int arr[], int n) {
-    // Build heap (rearrange array)
-    //i 是最後一個父節點
+    //i 是最後一個父節點，n是元素個數
     for (int i = n / 2 - 1; i >= 0; i--)
         heapify(arr, n, i);
-
-    // One by one extract an element from heap
-    for (int i = n - 1; i > 0; i--) {
-        // Move current root to end
+    //將最大值與最後一個元素交換，再重新做一次
+    for (int i = n-1; i > 0; i--) {
         swap(arr[0], arr[i]);
-
-        // call max heapify on the reduced heap
         heapify(arr, i, 0);
     }
 }
@@ -46,22 +39,17 @@ void printArray(int arr[], int n) {
 }
 
 int main() {
-    int ask=0;
-    while(ask==0)
-    {
-        int arr[6];
-        cout << "Enter 6 integers: ";
-        for (int i = 0; i < 6; i++) {
-            cin >> arr[i];
-        }
-        int n = sizeof(arr) / sizeof(arr[0]);
-
-        heapSort(arr, n);
-
-        cout << "Sorted array is \n";
-        printArray(arr, n);
-        cout << "Do you want to continue? (0/1): ";
-        cin >> ask;
+    int arr[6];
+    cout << "Enter 6 integers: ";
+    for (int i = 0; i < 6; i++) {
+        cin >> arr[i];
     }
+    //計算陣列大小
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    heapSort(arr, n);
+
+    cout << "Sorted array is \n";
+    printArray(arr, n);
     return 0;
 }
